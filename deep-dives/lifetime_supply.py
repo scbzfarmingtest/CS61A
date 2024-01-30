f42e13d28dc7f09e11ea3aee316b5d5512b9afb6
scbz_wsl_ubuntu <scbzfarmingtest@outlook.com>
2024-01-30 23:19:27 +0800

    Deployed cb7e9d4 with MkDocs version: 1.5.3

---
+++ deep-dives/lifetime_supply.py
@@ -0,0 +1,26 @@
+"""
+Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
+
+Write a function named calculate_supply that:
+* takes 2 arguments: age, amount per day.
+* calculates the amount consumed for rest of the life (based on a constant max age of 81).
+* returns the result
+"""
+
+# This def statement may be incomplete...
+def calculate_supply(year, amountPerDay):
+    """
+    >>> calculate_supply(80, 1)
+    365
+    >>> calculate_supply(80, 2)
+    730
+    >>> calculate_supply(36, 3)
+    49275
+    # Bonus: Accept floating point values for amount per day, and round the result up to an integer.
+    >>> calculate_supply(37, 2.17)
+    34850
+    """
+    # YPUR CODE HERE
+    return round((81 - year) * 365 * amountPerDay)
+
+print(calculate_supply(37, 2.17))
